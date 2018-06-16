92b66a5d92be0e5df05cabd130574ebbb68980bd
nami634 <cigarbox.nami@gmail.com>
2018-06-16 14:53:45 +0900

    オシレーターを作成

--- src/js/WebAudioTester.js
+++ src/js/WebAudioTester.js
@@ -1,6 +1,55 @@
 export default class {
-    constructor () {
+    constructor() {
+        performance.mark('constructor_start');
         console.log('hello');
+
+        this.context = new AudioContext();
+        this.current_time = 0;
+
+        this.gain_node = this.context.createGain();
+        this.gain_node.gain.value = 0.9;
+
+        this.gain_node.connect(this.context.destination);
+
+        this.play_osc = false;
+
+        this.initOsc();
+        performance.mark('constructor_end');
+        performance.measure("constructor", "constructor_start", "constructor_end");
+
+        console.log(performance.getEntries());
+    }
+
+    triggerOsc(_flag) {
+        this.play_osc = true;
+        if (_flag == "start") {
+            this.initOsc();
+            this.osc.start();
+        } else if (_flag == "stop") {
+            this.osc.stop();
+        }
+    }
+
+    changeGain(_gain) {
+        this.gain_node.gain.value = _gain;
+    }
+
+    initOsc() {
+        performance.mark('create_osc_start');
+        this.osc = this.context.createOscillator();
+        this.osc.connect(this.gain_node);
+        this.changeOscFreq(440);
+        this.changeOscType("sine");
+        performance.mark('create_osc_end');
+        performance.measure('create_osc', 'create_osc_start', 'create_osc_end');
+    }
+
+    changeOscFreq(_freq) {
+        this.osc.frequency.value = _freq;
+    }
+
+    changeOscType(_type) {
+        this.osc.type = _type;
     }
 }
 
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,25 @@
 import WebAudioTester from "./WebAudioTester.js";
 
 (function () {
-    let test = new WebAudioTester();
+    const test = new WebAudioTester();
+
+
+    // setTimeout(test.triggerOsc("start"),3000);
+
+    let play = document.querySelector("#play");
+    let stop = document.querySelector("#stop");
+    let change = document.querySelector("#change");
+
+    play.onclick = () => {
+        test.triggerOsc("start");
+    };
+
+    stop.onclick = () => {
+        test.triggerOsc("stop");
+    };
+
+    change.onclick = () => {
+        test.changeOscType("square");
+    };
+
 })();
